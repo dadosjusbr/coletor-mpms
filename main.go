11910b924b664ff58d28b08ec611373217cae8af
joaolgm <joao.menezes@ccc.ufcg.edu.br>
2022-04-12 21:04:18 -0300

    ajusta timeout

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	defaultGeneralTimeout  = 6 * time.Minute  // Duração máxima total da coleta de todos os arquivos. Valor padrão calculado a partir de uma média de execuções ~4.5min
-	defaulTimeBetweenSteps = 14 * time.Second //Tempo de espera entre passos do coletor."
+	defaultGeneralTimeout  = 6 * time.Minute // Duração máxima total da coleta de todos os arquivos. Valor padrão calculado a partir de uma média de execuções ~4.5min
+	defaulTimeBetweenSteps = 5 * time.Second //Tempo de espera entre passos do coletor."
 )
 
 func main() {
